8c21925c8ee15366956bd4de6db8e4e07f1a5f35
AKASH B <147173640+Akash242005@users.noreply.github.com>
2024-12-27 14:12:47 +0530

    Update Palindrom.java

--- Palindrom.java
+++ Palindrom.java
@@ -4,22 +4,23 @@ class palindrom{
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter the Sentence : ");
 		String str = sc.nextLine();
-		String temp = str;
 		int n = str.length();
 		int s = str.length()/2;
-		for(int i=0;i<s;i++)
+		char[] charector = str.toCharArray();
+		for(int i=0;i<n;i++)
 		{
-			char a = str.charAt(i)Gowtham;
-			str.replace(str.charAt(i),str.charAt(n-1-i));
-			str.replace(str.charAt(n-1-i),a);
+			char a  = charector[i];
+			charector[i] = charector[n-1-i];
+			charector[n-1-i] = a;
 		}
+		String temp = new String(charector);
 		if(str.equals(temp))
 		{
 			System.out.print("The given String is Palindrom!!! ans the string is : "+str);
 		}
 		else
 		{
-			System.out.print("The given String is Not Palindrom!!!");	
+			System.out.print("The given String is Not Palindrom!!! ans the Original String is : "+str+" and the Reversed String is : "+temp);	
 		}
 	}
 }
