907b3962d532b919ee91a19a7cde0bd687019aad
AKASH B <147173640+Akash242005@users.noreply.github.com>
2024-12-27 11:30:57 +0530

    Create String operations.java

---
+++ String operations.java
@@ -0,0 +1,38 @@
+import java.util.*;
+class Main{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		StringBuilder str = new StringBuilder();
+		str.append("Hello");
+		str.append(" World");
+		System.out.println("The String after appending is : "+str);
+		str.replace(5,6," to the ");
+		System.out.println("The String after Replacing is : "+str);
+		str.delete(1,3);
+		System.out.println("The String after Deleting is : "+str);
+		str.reverse();
+		System.out.println("The String after Reversing is : "+str);
+		System.out.println("The String capacity is : "+(str.capacity()));
+		System.out.println("==========String Builder up=======");
+		System.out.println(" ");
+		System.out.println("==========Strings down============");
+		String str1 = "      Normal String ";
+		System.out.println("The String Length is : "+(str1.length()));
+		System.out.println("The indexOf operation  : "+(str1.indexOf('S')));
+		System.out.println("The charAt operation  : "+(str1.charAt(4)));
+		String str2 = "String";
+		System.out.println("The compare operation  : "+(str2.compareTo("sTRING")));
+		System.out.println("The compare without case operation  : "+(str2.compareToIgnoreCase("string")));
+		System.out.println("The contains operation  : "+(str1.contains("mal ")));
+		System.out.println("The  endwith operation  : "+(str1.endsWith("g ")));
+		System.out.println("The replace operation  : "+(str1.replace("String ","Programming is Java")));
+		System.out.println("The replaceall operation  : "+(str1.replaceAll(" ","@!")));
+		System.out.println("The replace first operation  : "+(str1.replaceFirst("[ ]","\\$")));
+		System.out.println("The lower case operation  : "+(str1.toLowerCase()));
+		System.out.println("The upper case operation  : "+(str1.toUpperCase()));
+		System.out.println("The trim operation  : "+(str1.trim()));
+		System.out.println("The number to string  : "+(String.valueOf(123456789)));
+		char[] array = str1.toCharArray();
+		System.out.println("The character array is  : "+(Arrays.toString(array)));
+	}
+}
